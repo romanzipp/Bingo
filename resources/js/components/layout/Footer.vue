21d4c37adc774790d773226d974872c0a190f1cf
Roman Zipp <ich@ich.wtf>
2020-10-18 22:18:05 +0200

    Add footer open source info

--- resources/js/components/layout/Footer.vue
+++ resources/js/components/layout/Footer.vue
@@ -2,7 +2,7 @@
 
     <footer class="mt-64">
 
-        <div class="container mx-auto px-4 py-6">
+        <div class="container mx-auto px-4 py-6 flex justify-between">
 
             <div class="flex space-x-4 text-sm">
 
@@ -12,6 +12,14 @@
 
                 <a href="https://ich.wtf/privacy" target="_blank" class="text-gray-600 dark:text-gray-400">
                     Privacy
+                </a>
+
+            </div>
+
+            <div class="flex space-x-4 text-sm">
+
+                <a href="https://github.com/romanzipp/Bingo" target="_blank" rel="noopener" class="text-gray-600 dark:text-gray-400">
+                    This Project is Open Source
                 </a>
 
             </div>
